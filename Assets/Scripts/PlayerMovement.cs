a6172a1b076807dc1ba01277b4fb9b829e874667
Atmerson <atmerson99@gmail.com>
2020-10-11 14:22:24 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rb;
+
+    private float mx;
+    public float jumpForce = 20f;
+    private void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+        rb.velocity = movement;
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x , jumpForce);
+
+        rb.velocity = movement;
+    }
+    
+}
